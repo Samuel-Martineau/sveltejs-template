eefcf2eeb2998bf3af38e64656bd05ca120ae106
Samuel-Martineau <samumartineau@gmail.com>
2019-10-18 22:55:49 -0400

    Finalisation du gabarit

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,14 @@
 <script>
-	export let name;
+  export let name = '';
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
+<style lang="scss" global>
+  h1 {
+    color: purple;
+    &:hover {
+      color: blue;
+    }
+  }
 </style>
 
-<h1>Hello {name}!</h1>
+<h1>Hello {name}</h1>
